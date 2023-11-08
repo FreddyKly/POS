8f62ebc53ed2d014000678c42b888ae3c2957956
FreddyKly <Frederik.Kliemt@web.de>
2023-11-08 20:16:39 +0100

    3.1 works

--- src/main/java/Exercises/C/tester.java
+++ src/main/java/Exercises/C/tester.java
@@ -3,7 +3,8 @@ package Exercises.C;
 public class tester {
     public static void main(String[] args) {
         Const first = new Const(3);
-        Const second = new Const(5);
-        System.out.println(new Sum(first, second).eval().toString());
+        Const second = new Const(4);
+        Const third = new Const(5);
+        System.out.println(new Prod(new Sum(first, second).eval(), third).eval().toString());
     }
 }
